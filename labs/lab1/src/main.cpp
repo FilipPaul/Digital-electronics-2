822a3eea50089ac768c9b17a7497ccb00cf0f3c3
FIlip Paul <filip.paul@email.cz>
2020-09-23 21:35:51 +0200

    laborky 1

---
+++ labs/lab1/src/main.cpp
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+
+/***********************************************************************
+ * 
+ * Blink a LED and use the function from the delay library.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2018-2020 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+/* Defines -----------------------------------------------------------*/
+#define LED_GREEN PB5   // AVR pin where green LED is connected
+#define SHORT_DELAY 300 // Delay in miliseconds
+#define LONG_DELAY 600
+#define BETWEEN_DELAY 300
+#ifndef F_CPU
+#define F_CPU 16000000 // CPU frequency in Hz required for delay func
+#endif
+
+/* Includes ----------------------------------------------------------*/
+#include <util/delay.h> // Functions for busy-wait delay loops
+#include <avr/io.h>     // AVR device-specific IO definitions
+void morse(String letter)
+{
+  if (letter == "D")
+  {
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+  }
+  else if (letter == "E")
+  {
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+  }
+  else if (letter == "2")
+  {
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(SHORT_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+    PORTB = PORTB ^ (1 << LED_GREEN);
+    _delay_ms(LONG_DELAY);
+  }
+}
+
+/* Variables ---------------------------------------------------------*/
+
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+/**
+ * Toggle one LED and use the function from the delay library.
+ */
+int main(void)
+{
+  // Set pin as output in Data Direction Register
+  // DDRB = DDRB or 0010 0000
+  DDRB = DDRB | (1 << LED_GREEN);
+
+  // Set pin LOW in Data Register (LED off)
+  // PORTB = PORTB and 1101 1111
+  PORTB = PORTB & ~(1 << LED_GREEN);
+
+  // Infinite loop
+  while (1)
+  {
+    morse("D");
+    _delay_ms(BETWEEN_DELAY);
+    morse("E");
+    _delay_ms(BETWEEN_DELAY);
+    morse("2");
+    _delay_ms(BETWEEN_DELAY);
+  }
+
+  // Will never reach this
+  return 0;
+}
+
+
+/* Interrupt routines ------------------------------------------------*/
