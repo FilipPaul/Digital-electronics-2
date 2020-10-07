0b81684e018d468c4ea7025f91e63b9a6b759124
unknown <filip.paul@email.cz>
2020-10-07 14:44:24 +0200

    lab 3 upload

--- labs/lab3/src/main.cpp
+++ labs/lab3/src/main.cpp
@@ -1,9 +1,50 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+/* Defines -----------------------------------------------------------*/
+#define LED_GREEN   PB5     // AVR pin where green LED is connected
+#define BLINK_DELAY 500
+#define BTN         PD5
+#define LED_RED     PB4
+#ifndef F_CPU
+#define F_CPU 16000000      // CPU frequency in Hz required for delay
+#endif
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/* Includes ----------------------------------------------------------*/
+#include <util/delay.h>     // Functions for busy-wait delay loops
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <gpio.h>          // GPIO library for AVR-GCC
+
+int main(void)
+{
+    /* GREEN LED */
+    GPIO_config_output(&DDRB, LED_GREEN);
+    GPIO_write_low(&PORTB, LED_GREEN);
+
+    /* RED LED */
+    GPIO_config_output(&DDRB, LED_RED);
+    GPIO_write_low(&PORTB, LED_RED);
+
+    /* push button */
+    GPIO_config_input_pullup(&DDRD, BTN);
+
+    // Infinite loop
+    while (1)
+    {
+        // Pause several milliseconds
+        _delay_ms(BLINK_DELAY);
+        if (!GPIO_read(&PIND,BTN))
+        {
+          GPIO_toggle(&PORTB,LED_GREEN);//toggle leds
+          GPIO_toggle(&PORTB,LED_RED);
+        }
+        if (GPIO_read(&PIND,BTN))
+        {
+          GPIO_write_low(&PORTB,LED_GREEN);//turn off if button isnt pushed
+          GPIO_write_low(&PORTB,LED_RED);
+        }
+
+    }
+
+    // Will never reach this
+    return 0;
 }
